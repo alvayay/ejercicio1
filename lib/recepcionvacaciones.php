2855593d94a5469033f52a8fcd8e1f3bed6e3b54
cetecom <cetecom@LCV10813>
2019-03-18 22:28:54 -0300

    primer proyect

---
+++ lib/recepcionvacaciones.php
@@ -0,0 +1,25 @@
+<?php
+include('constantes.php');
+include('vacaciones.php');
+
+$oVacacion = new Vacaciones($_POST["rut"], $_POST["nombre"], $_POST["cargo"],"" , "", "");
+
+
+if(isset($_SESSION[$aVacaciones])){
+    $arrVacaciones = $_SESSION["aVacaciones"];
+    
+}
+
+$aVacaciones[] = $oVacacion;
+$_SESSION["aVacaciones"] = $arrVacaciones;
+
+echo "<pre>";
+var_dump($_POST);
+echo "</pre>";
+
+echo "<pre>";
+var_dump($_SESSION["aVacaciones"]);
+echo "</pre>";
+
+
+
---
+++ lib/registro.php
@@ -0,0 +1,6 @@
+<?php
+
+
+class registro {
+    //put your code here
+}
